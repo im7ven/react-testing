068e9475942ae2447a8f3dc959775e72af072f07
Justin <alexanderrr416@gmail.com>
2024-11-05 20:04:20 -0500

    chore: setup layout component

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-const router = createBrowserRouter([{ path: "/", element: <HomePage /> }]);
+import Layout from "./components/Layout";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [{ index: true, element: <HomePage /> }],
+  },
+]);
 
 export default router;
