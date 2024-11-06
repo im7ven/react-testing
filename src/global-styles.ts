282fbc59841d312f86d6945e15d7c9e7ca84ff43
Justin <alexanderrr416@gmail.com>
2024-11-05 22:45:05 -0500

    chore: add some global styles

--- src/global-styles.ts
+++ src/global-styles.ts
@@ -12,6 +12,10 @@ body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: 'Poppins', Arial, Helvetica, sans-serif;
+    max-width: 1200px;
+    margin: 0 auto;
+
 }
 `;
 
