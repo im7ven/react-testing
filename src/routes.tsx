68a5725aca5d9fc726708baa4eb16e4a35a78757
Justin <alexanderrr416@gmail.com>
2024-11-05 19:58:55 -0500

    chore: remove unused code

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import HomePage from "./HomePage";
+import HomePage from "./pages/HomePage";
 const router = createBrowserRouter([{ path: "/", element: <HomePage /> }]);
 
 export default router;
