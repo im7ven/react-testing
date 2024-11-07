8d6dcbef90a83368ccb83bfcf4c23e65b35946f2
Justin <alexanderrr416@gmail.com>
2024-11-06 21:01:24 -0500

    feat: add todo page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Layout from "./components/Layout";
+import TodoPage from "./pages/Todo";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    children: [{ index: true, element: <HomePage /> }],
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "/apps/todo", element: <TodoPage /> },
+    ],
   },
 ]);
 
