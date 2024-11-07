7f2e212277cdd72761f31e3b6ca6e90821998846
Justin <alexanderrr416@gmail.com>
2024-11-06 22:47:14 -0500

    chore: remove padding and margin from all elements

--- src/global-styles.ts
+++ src/global-styles.ts
@@ -2,6 +2,11 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
 
+* {
+    padding: 0;
+    margin:0;
+}
+
 html {
     scroll-behavior: smooth;
     font-size: 62.5%;
