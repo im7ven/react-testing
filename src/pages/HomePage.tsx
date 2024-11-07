1cce7c8c79f2eccae6aa17a2549fa97e1dcf0ade
Justin <alexanderrr416@gmail.com>
2024-11-06 21:01:11 -0500

    feat: build home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,62 @@
+import { RiTodoLine } from "react-icons/ri";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import TechCard from "../components/ProjectCard";
+
+const Headline = styled.h1`
+  font-size: 4rem;
+  max-width: 60rem;
+  margin: 3rem auto 0;
+`;
+
+const Highlight = styled.span`
+  background: linear-gradient(to right, #0263ff 0%, #0098cf 100%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`;
+
+const ProjectGrid = styled.div`
+  display: grid;
+  place-content: center;
+  grid-template-columns: repeat(2, 30rem);
+  gap: 1rem;
+  margin: 6rem auto 0;
+`;
+
+const ProjectLink = styled(Link)`
+  text-decoration: none;
+  color: #fff;
+`;
+
 function App() {
-  return <main></main>;
+  return (
+    <>
+      <Headline>
+        Building small apps to focus on <Highlight>testing</Highlight> and
+        quality
+      </Headline>
+      <ProjectGrid>
+        <ProjectLink to="/apps/todo">
+          <TechCard
+            icon={<RiTodoLine />}
+            description=" A simple todo app featuring a user-friendly experience"
+          />
+        </ProjectLink>
+        <TechCard
+          icon={<RiTodoLine />}
+          description=" A simple todo app featuring a user-friendly experience"
+        />
+        <TechCard
+          icon={<RiTodoLine />}
+          description=" A simple todo app featuring a user-friendly experience"
+        />
+        <TechCard
+          icon={<RiTodoLine />}
+          description=" A simple todo app featuring a user-friendly experience"
+        />
+      </ProjectGrid>
+    </>
+  );
 }
 
 export default App;
