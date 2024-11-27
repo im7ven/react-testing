cd68939b8f2564171dfa4df0a537463fe0d19732
Justin <alexanderrr416@gmail.com>
2024-11-26 22:57:55 -0500

    feat: filter expenses by category

--- src/pages/ExpenseTracker.tsx
+++ src/pages/ExpenseTracker.tsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import ExpenseFormModal, {
   FormData,
 } from "../components/expense-tracker/ExpenseFormModal";
 import { Expense } from "../types";
 import { Button } from "../utility-styles";
+import ExpenseCard from "../components/expense-tracker/ExpenseCard";
+import ExpenseSelector from "../components/expense-tracker/ExpenseSelector";
 
 const ExpenseTotal = styled.span`
   color: ${(props) => props.theme.color.primaryText};
@@ -15,6 +17,14 @@ const OptionsBar = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  padding-bottom: 1rem;
+  border-bottom: ${(props) => props.theme.border};
+`;
+
+const ExpenseGrid = styled.section`
+  display: flex;
+  gap: 1rem;
+  padding: 2rem -0;
 `;
 
 const defaultExpenses: Expense[] = [
@@ -41,6 +51,7 @@ const defaultExpenses: Expense[] = [
 const ExpenseTrackerPage = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [expenses, setExpenses] = useState<Expense[]>(defaultExpenses);
+  const [category, setCategory] = useState("");
 
   const handleOpenModal = () => {
     setIsModalOpen(true);
@@ -50,7 +61,13 @@ const ExpenseTrackerPage = () => {
     setIsModalOpen(false);
   };
 
-  console.log(expenses);
+  const handleChangeCategory = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setCategory(e.target.value);
+  };
+
+  const filteredExpenses = category
+    ? expenses.filter((expense) => expense.category === category)
+    : expenses;
 
   return (
     <div>
@@ -70,11 +87,20 @@ const ExpenseTrackerPage = () => {
       )}
       <OptionsBar>
         <ExpenseTotal>
-          {/* {`$${expenses.reduce((acc, val) => acc + val.amount, 0).toFixed(2)}`} */}
+          {`$${filteredExpenses
+            ?.reduce((acc, val) => acc + val.amount, 0)
+            .toFixed(2)}`}
         </ExpenseTotal>
-
-        <Button onClick={handleOpenModal}>Add Expense</Button>
+        <div>
+          <ExpenseSelector onChange={handleChangeCategory} />
+          <Button onClick={handleOpenModal}>Add Expense</Button>
+        </div>
       </OptionsBar>
+      <ExpenseGrid>
+        {filteredExpenses.map((expense) => (
+          <ExpenseCard key={expense.title} {...expense} />
+        ))}
+      </ExpenseGrid>
     </div>
   );
 };
