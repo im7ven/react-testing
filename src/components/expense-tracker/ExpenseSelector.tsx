aac6d1140c779eb644f76d65529a2055174451ff
Justin <alexanderrr416@gmail.com>
2024-11-26 22:57:15 -0500

    feat: build expense selector component

---
+++ src/components/expense-tracker/ExpenseSelector.tsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+const Selector = styled.select`
+  border: ${(props) => props.theme.border};
+  border-radius: 5px;
+  padding: 1rem 1.8rem;
+  background: none;
+  color: ${(props) => props.theme.color.primaryText};
+  margin-right: 1rem;
+`;
+
+const Option = styled.option`
+  background-color: ${(props) => props.theme.color.background};
+  color: ${(props) => props.theme.color.primaryText};
+
+  &:hover {
+    background-color: red;
+  }
+`;
+
+interface Props {
+  onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+}
+
+const ExpenseSelector = ({ onChange }: Props) => {
+  return (
+    <Selector onChange={(e) => onChange(e)} defaultValue="placeholder">
+      <Option value="placeholder" disabled>
+        Select Category
+      </Option>
+      <Option value="Food">Food</Option>
+      <Option value="Vehicle">Vehicle</Option>
+      <Option value="Fun">Fun</Option>
+    </Selector>
+  );
+};
+
+export default ExpenseSelector;
