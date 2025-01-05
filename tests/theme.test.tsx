f4a49e3e37b72b12df9050ea4e1d2bd01451c343
Justin <alexanderrr416@gmail.com>
2025-01-05 15:35:55 -0500

    chore: create test for theme

--- tests/theme.test.tsx
+++ tests/theme.test.tsx
@@ -1,6 +1,24 @@
-import { it, expect, describe } from "vitest";
-describe("Testing", () => {
-  it("should", () => {
-    expect(1).toBeTruthy();
-  });
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { MemoryRouter } from "react-router-dom";
+import Navbar from "../src/components/Navbar";
+import { ThemeContextProvider, useTheme } from "../src/contexts/ThemeContext";
+import { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme } from "../src/theme";
+
+describe("Theme", () => {
+  it("should not render the theme toggle if user is on home page", () => {
+    render(
+      <ThemeContextProvider>
+        <MemoryRouter initialEntries={["/apps"]}>
+          <ThemeProvider theme={lightTheme}>
+            <Navbar />
+          </ThemeProvider>
+        </MemoryRouter>
+      </ThemeContextProvider>
+    );
+
+    const toggleWrapper = screen.queryByTestId("theme-toggle");
+    expect(toggleWrapper).not.toBeInTheDocument();
+  }); 
 });
