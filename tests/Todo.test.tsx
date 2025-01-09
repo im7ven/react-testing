3e6ca7fd8ba59d1c3246bf76d07372194082c4fb
Justin <alexanderrr416@gmail.com>
2025-01-08 19:52:03 -0500

    chore: write all test for adding/validating todos

--- tests/Todo.test.tsx
+++ tests/Todo.test.tsx
@@ -5,25 +5,59 @@ import { ThemeProvider } from "styled-components";
 import { lightTheme } from "../src/theme";
 
 describe("Todo - Adding Todos", () => {
-  it("should add a todo if a valid description is provided", async () => {
+  const renderTodoPage = () => {
     render(
       <ThemeProvider theme={lightTheme}>
         <TodoPage />
       </ThemeProvider>
     );
 
-    const button = screen.getByRole("button", { name: /add todo/i });
-    const input = screen.getByPlaceholderText(/e.g./);
+    return {
+      button: screen.getByRole("button", { name: /add/i }),
+      input: screen.getByPlaceholderText(/e.g./),
+      user: userEvent.setup(),
+      duplicateTodo: "New Todo",
+    };
+  };
+
+  it("should add a todo if a valid description is provided", async () => {
+    const { button, input, user } = renderTodoPage();
 
     expect(button).toBeInTheDocument();
     expect(input).toBeInTheDocument();
 
-    const user = userEvent.setup();
     await user.type(input, "New Todo");
     await user.click(button);
 
     const todo = screen.getByText("New Todo");
 
     expect(todo).toBeInTheDocument();
+  });
+
+  it("should prevent duplicate todos from being added ", async () => {
+    const { button, user, input, duplicateTodo } = renderTodoPage();
+
+    await user.type(input, duplicateTodo);
+    await user.click(button);
+
+    await user.type(input, duplicateTodo);
+    await user.click(button);
+
+    expect(screen.getAllByText(duplicateTodo).length).toBe(1);
+  });
+
+  it("should render the correct error message when validating todos", async () => {
+    const { button, user, input, duplicateTodo } = renderTodoPage();
+
+    await user.click(button);
+
+    expect(screen.getByText("Todo is required")).toBeInTheDocument();
+
+    await user.type(input, duplicateTodo);
+    await user.click(button);
+    await user.type(input, duplicateTodo);
+    await user.click(button);
+
+    expect(screen.getByText("Todo already exist")).toBeInTheDocument();
   });
 });
